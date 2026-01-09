e86f5019e5e8539d61ec4e949e7c30243c1c00b4
Konda Vinay Prasanna Jitesh <kondavinay52@gmail.com>
2026-01-09 19:55:15 +0300

    Create HI.java

---
+++ HI.java
@@ -0,0 +1,15 @@
+class Shape {
+    void draw() {
+        System.out.println("Drawing shape");
+    }
+}
+class Circle extends Shape {
+    void drawCircle() {
+        System.out.println("Drawing circle");
+    }
+}
+class Rectangle extends Shape {
+    void drawRectangle() {
+        System.out.println("Drawing rectangle");
+    }
+}
